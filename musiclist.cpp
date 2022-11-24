b55794334cbc11e2847cbbe8c1560e3316f534de
shiyue <2422645737@qq.com>
2022-11-24 11:11:45 +0800

    '打开文件和文件夹功能完成'

--- musiclist.cpp
+++ musiclist.cpp
@@ -1,18 +1,37 @@
 #include "musiclist.h"
 
+MusicList::MusicList(QWidget *parent) : QWidget(parent)
+{
+    init();      //初始化
+}
+
 void MusicList::init()
 {
+    player = new QMediaPlayer(this);
+    music_list = new QVector<Music>;
     initUI();
 
     //初始化工作
     initConnect();
-
 }
 
 void MusicList::initUI()
 {
     setColor(this);
-    this->list_widget = new QListWidget(this);
+//    this->list_widget = new QListWidget(this);
+    this->list_widget = new QTableWidget(this);
+    this->list_widget->setColumnCount(3);       //初始化列数
+    this->list_widget->setRowCount(0);          //初始化行数
+    this->list_widget->verticalHeader()->setVisible(false);         //隐藏行号
+    this->list_widget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);         //行自动拉伸长度
+    this->list_widget->setSelectionBehavior(QTableWidget::SelectRows);       //设置一次只能选中一行
+    list_widget->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);     //保证控件充满整个布局
+
+    QStringList name;
+    name.append("序号");
+    name.append("歌曲名");
+    name.append("时长");
+    list_widget->setHorizontalHeaderLabels(name);
 
     //按钮栏
     add_file_button = new QPushButton(this);
@@ -36,46 +55,43 @@ void MusicList::initConnect()
 {
     connect(this->add_file_button,&QPushButton::clicked,this,&MusicList::addFile);        //添加文件
     connect(this->add_folder_button,&QPushButton::clicked,this,&MusicList::addFolder);        //添加文件夹
+    connect(this->player,&QMediaPlayer::metaDataAvailableChanged,this,[&](bool available){
+        if(available){
+            this->music.duration = player->duration();
+            music_list->replace(music_list->size() - 1,this->music);
+            refresh_list();
+        }
+    });
 }
 
 
-void MusicList::refresh_list(QString path,int flag = 0)       //flag = 0（添加文件）    1（添加文件夹）
+void MusicList::refresh_list()       //flag = 0（添加文件）    1（添加文件夹）
 {
-//    QFileInfo file_info(path);
-//    qDebug() << "-----------------";
-//    qDebug() << file_info.baseName();
-//    qDebug() << file_info.fileName();
-//    qDebug() << file_info.filePath();
-//    qDebug() << "-----------------";
-    if(flag == 0){       //添加文件
-
-    }else if(flag == 1){        //添加文件夹
-
+    this->list_widget->setRowCount(0);       //清空列表
+    print_list(music_list);
+    for(auto it = music_list->begin();it != music_list->end();it++){
+        this->list_widget->setRowCount(this->list_widget->rowCount() + 1);       //添加新行
+        this->list_widget->setItem(this->list_widget->rowCount() - 1,0,new QTableWidgetItem(QString::number(it - music_list->begin())));
+        this->list_widget->setItem(this->list_widget->rowCount() - 1,1,new QTableWidgetItem(it->name));
+        this->list_widget->setItem(this->list_widget->rowCount() - 1,2,new QTableWidgetItem(getDuration(it->duration)));
     }
 }
 
-Music MusicList::pack_music(QString path)
+void MusicList::packMusic(QString file)
 {
-    Music music;
-    QFileInfo file_info(path);
-    music.path = path;
-    music.name = file_info.baseName();
-    music.suffix = file_info.suffix();
-    QMediaPlayer* player = new QMediaPlayer;
-    player->setMedia(QUrl::fromLocalFile(path));
-//    connect(player,&QMediaPlayer::durationChanged,this,&MusicList::setDuration);
-    connect(player,&QMediaPlayer::durationChanged,this,[&](qint64 duration){
-        this->temp = new Music;
-        this->temp->duration = duration;
-        print_music(*this->temp);
-    });
-//    print_music(music);
-    return music;
+    QFileInfo info(file);
+    music.name = info.baseName();
+    music.path = info.filePath();
+    music.suffix = info.suffix();        //封装music信息
+    music_list->append(music);
+    player->setMedia(QUrl::fromLocalFile(file));          //修改媒体
 }
 
-MusicList::MusicList(QWidget *parent) : QWidget(parent)
+QString MusicList::getDuration(qint64 duration)
 {
-    init();      //初始化
+    QTime time(0,0,0,0);
+    time = time.addMSecs(duration);
+    return time.toString("hh:mm:ss");
 }
 
 void MusicList::addFile()
@@ -87,33 +103,48 @@ void MusicList::addFile()
     }
     //检测是否重复
     bool flag = false;
-    for(auto it = music_list.begin();it != music_list.end();it++){
-        if(it->path == file){
-            qDebug() << "该音乐已经存在";
+    for(auto it = this->music_list->begin();it != this->music_list->end();it++){
+        if(it->path == file){          //当前歌曲已经存在
             flag = true;
         }
     }
-    if(flag == false){
-        qDebug() << "fllllllllllllllll" << file;
-        pack_music(file);
+    if(flag == false){       //将music添加到music_list
+        packMusic(file);
+
+    }else{
+        qDebug() << "歌曲已经存在于列表中";
+        QMessageBox::information(this,"提示","该音乐已经存在于列表中");
     }
-
-    emit music_changed(file);
-
 }
 
 void MusicList::addFolder()
 {
-    QString file = QFileDialog::getExistingDirectory(this,tr("打开文件夹"),"/",QFileDialog::ShowDirsOnly);
-    if(file == nullptr){
+    QString path = QFileDialog::getExistingDirectory(this,tr("打开文件夹"),"/",QFileDialog::ShowDirsOnly);
+    if(path == nullptr){
         qDebug() << "用户未选择文件夹";
         return;
     }
-    //刷新列表
-    emit music_changed(file);
-}
+    //寻找路径下所有音乐文件
+   QDir dir(path);
+   QStringList filter;
+   filter << "*.mp3";         //获取所有mp3格式的文件
+   QStringList result = dir.entryList(filter,QDir::Files | QDir::Readable);
 
-void MusicList::setDuration(qint64 duration)
-{
-
+   //对音乐文件进行查重
+   for(auto it = result.begin();it != result.end();it++){
+       QString current_path = path + "/" +  *it;         //获取当前文件夹下音频文件的完整路径
+       bool flag = false;
+       for(auto i = this->music_list->begin();i != this->music_list->end();i++){
+           if(current_path == i->path){
+               //发现重复
+               flag  = true;
+               break;
+           }
+       }
+       if(!flag){      //未发现重复歌曲，插入到music_list中
+           packMusic(current_path);
+       }else{
+           //do nothing
+       }
+   }
 }
--- musiclist.h
+++ musiclist.h
@@ -3,33 +3,44 @@
 
 #include <QWidget>
 #include "common.h"
+#include <QTableWidgetItem>
 #include <QListWidget>
+#include <QTableWidget>
+//#include <QTableView>
+#include <QHeaderView>
+#include <QAbstractItemView>
 
 class MusicList : public QWidget
 {
     Q_OBJECT
 private:
-    QListWidget* list_widget;      //列表
+    QMediaPlayer* player;
+//    QListWidget* list_widget;      //列表
+    QTableWidget* list_widget;
     QPushButton* add_file_button;    //打开文件
     QPushButton* add_folder_button;   //打开文件夹
-    QVector<Music> music_list;        //音乐数组
-    Music* playing_music;      //正在播放的音乐
+    QVector<Music>* music_list;     //音乐列表
+    Music music;
 
+
+
+    /* 功能函数 */
     void init();
-    void initUI();
+    void initUI();       //初始化UI界面
     void initConnect();     //初始化connect连接
-    void refresh_list(QString path,int flag);     //刷新列表
-    Music pack_music(QString path);         //根据路径封装struct Music
+    void refresh_list();     //刷新列表
+    void packMusic(QString path);      //封装歌曲信息，包含了插入到music_list和刷新列表功能
+
+    QString getDuration(qint64 duration);   //毫秒转化为分钟格式
 public:
     explicit MusicList(QWidget *parent = nullptr);
-    Music* temp;          //临时音乐变量
 public slots:
     void addFile();
     void addFolder();
-    void setDuration(qint64 duration);
 
 signals:
     void music_changed(QString path);
+    void meta_changed(QString path);
 };
 
 #endif // MUSICLIST_H
